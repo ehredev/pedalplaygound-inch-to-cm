b5ca441ce4fd4df213e9f60a93c03b6821d1d032
Junyong Choi <pjs99011@gmail.com>
2024-10-08 20:11:41 +0900

    매뉴 제작 및 mm 대응

--- content.js
+++ content.js
@@ -1,25 +1,42 @@
 function changeUnit() {
     setTimeout(function () {
-        try{
-            var panel = document.getElementsByClassName("panel__dimensions")[0].innerText;
-            if (panel && !panel.includes("cm")) {
-                const original = panel; 
-                const text = panel.slice(1, -1);
-                var arr = text.replaceAll("in", "").split(" x ");
-                for (var i = 0; i < arr.length; i++) {
-                    arr[i] = Number(arr[i] * 2.54).toFixed(2) + "cm";
-                }
-                var content = original + "\n("+arr.join(" x ")+")";
-                document.getElementsByClassName("panel__dimensions")[0].innerText = content;
-            }
-        }   catch (error) {}
-            setTimeout(changeUnit, 100);
-        }, 100)
-}
+        try {
+            if (document.querySelector("#convert-units-cm").checked) {
+                var panel = document.getElementsByClassName("panel__dimensions")[0].innerText;
+                if (panel && !panel.includes("cm")) {
+                    const original = panel;
+                    const text = panel.slice(1, -1);
+                    var arr = text.replaceAll("in", "").replaceAll("mm", "").split(" x ");
+                    for (var i = 0; i < arr.length; i++) {
+                        if (panel.includes("in")) {
+                            arr[i] = Number(arr[i] * 2.54).toFixed(2) + "cm";
+                        } else if (panel.includes("mm")) {
+                            arr[i] = Number(arr[i] / 10).toFixed(2) + "cm";
+                        }
+                    }
+                    if (document.querySelector("#convert-units").checked) {
 
+                        var content = original + "\n (" + arr.join(" x ") + ")";
+                    } else {
+                        var content = "(" + arr.join(" x ") + ")";
+                    }
+                    document.getElementsByClassName("panel__dimensions")[0].innerText = content;
+                }
+            }
+        } catch (error) { }
+        setTimeout(changeUnit, 50);
+    }, 50)
+}
 
 document.addEventListener('click', function (event) {
     if (window.location.href.startsWith("https://pedalplayground.com/")) {
         changeUnit();
     }
 }, true);
+
+
+if (window.location.href.startsWith("https://pedalplayground.com/")) {
+    console.log("Pedal Playground");
+    document.querySelector('.panel__action').outerHTML += '<div class="panel__action"><label for="convert-units" class="convert-units">Use cm for units</label><input id="convert-units-cm" type="checkbox"></div>';
+
+}
